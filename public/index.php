f5eecad92c9bee6581dd1b4a289ffbbf7a2ed08e
ME.AT <meatish2008@gmail.com>
2025-03-11 12:58:21 +0330

    feat: create basic router

--- public/index.php
+++ public/index.php
@@ -2,7 +2,18 @@
 require "../helper.php";
 
 
-loadView('homepage');
+$routes = [
+    "/" => "controller/home.php",
+    "/listing" => "controller/listing/index.php",
+    "/listing/create" => "controller/listing/create.php",
+    "404" => "controller/errors/404.php"
+];
 
-?>
 
+$uri = $_SERVER["REQUEST_URI"];
+
+if (array_key_exists($uri, $routes)) {
+    require basePath($routes[$uri]);
+} else {
+    require basePath($routes["404"]);
+}
