a479ade9f1c037bbc128cda0f00fff79050f5373
ME.AT <meatish2008@gmail.com>
2025-03-11 13:32:39 +0330

    ref: separate Router file

--- public/index.php
+++ public/index.php
@@ -1,19 +1,6 @@
 <?php
 require "../helper.php";
 
-
-$routes = [
-    "/" => "controller/home.php",
-    "/listing" => "controller/listing/index.php",
-    "/listing/create" => "controller/listing/create.php",
-    "404" => "controller/errors/404.php"
-];
-
-
 $uri = $_SERVER["REQUEST_URI"];
 
-if (array_key_exists($uri, $routes)) {
-    require basePath($routes[$uri]);
-} else {
-    require basePath($routes["404"]);
-}
+require basePath("router.php");
---
+++ routes.php
@@ -0,0 +1,8 @@
+<?php
+
+return  [
+    "/" => "controller/home.php",
+    "/listing" => "controller/listing/index.php",
+    "/listing/create" => "controller/listing/create.php",
+    "404" => "controller/errors/404.php"
+];
