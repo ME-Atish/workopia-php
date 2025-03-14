400e4dd60dd8e1fef62e669073261b69b56a1cd8
ME.AT <meatish2008@gmail.com>
2025-03-14 13:21:51 +0330

    feat: create query method at Database

--- controller/home.php
+++ controller/home.php
@@ -1,3 +1,8 @@
 <?php
+$config = require basePath("config/db.php");
+
+$db = new Database($config);
+
+$listing = $db->query("SELECT * FROM listing")->fetchAll();
 
 loadView("homepage");
--- public/index.php
+++ public/index.php
@@ -2,9 +2,7 @@
 require "../helper.php";
 require basePath("Router.php");
 require basePath("Database.php");
-$config = require basePath("config/db.php");
 
-$db = new Database($config);
 $router = new Router();
 $routes = require basePath("routes.php");
 
